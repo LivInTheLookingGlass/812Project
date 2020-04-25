720ced2436b0098dc56eeff1825f14475ec66350
Gabe Appleton <gabe@gabeappleton.me>
2020-04-24 20:24:02 -0400

    Fix grader format

--- accuracy_grader.py
+++ accuracy_grader.py
@@ -6,7 +6,7 @@ from sys import argv
 bucketized = argv[-2]
 comparison = argv[-1]
 
-correct = total = 0
+oracle_correct = causal_correct = wrong = 0
 
 with open(bucketized, "r") as f:
     buckets = json.load(f)
@@ -19,7 +19,10 @@ for idx, bucket_1 in enumerate(buckets):
         for x in bucket_1:
             for y in bucket_2:
                 if events.index(x) < events.index(y):
-                    correct += 1
-                total += 1
+                    oracle_correct += 1
+                elif not (all(a >= b for a, b in zip(x['vc'], y['vc'])) and any(a > b for a, b in zip(x['vc'], y['vc']))):  # first test is less expensive, but has false negatives
+                    causal_correct += 1
+                else:
+                    wrong += 1
 
-print(f"{correct:,}/{total:,} = {correct/total:.02%}")
+print(f"{oracle_correct:,} / {causal_correct:,} / {wrong:,}")
